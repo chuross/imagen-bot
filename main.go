a41d9db7b7d7aa00d44d047f5e2e23e476463935
chuross <azarasi.mo.mo@gmail.com>
2022-09-08 12:06:51 +0000

    add gin

--- main.go
+++ main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	r := gin.Default()
+
+	r.POST("/hook", func(ctx *gin.Context) {
+		ctx.Status(http.StatusOK)
+	})
+
+	r.Run()
 }
