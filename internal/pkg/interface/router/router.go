aa75541b8f2ca53f0b8f2f343f4fd56460946901
chuross <azarasi.mo.mo@gmail.com>
2022-09-11 01:35:14 +0000

    line webhook endpoint

--- internal/pkg/interface/router/router.go
+++ internal/pkg/interface/router/router.go
@@ -14,6 +14,6 @@ func Setup(r *gin.Engine, webhookUseCases webhook.UseCases) {
 
 	{
 		handler := handler.NewWebhookHandler(webhookUseCases)
-		root.POST("/hook", handler.Hook)
+		root.POST("/hooks/line", handler.Hook)
 	}
 }
